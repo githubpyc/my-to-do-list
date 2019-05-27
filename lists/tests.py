502989a3e5d6e44fa8aafab8f7bcf0ea1d9048e1
elon <awesoempyc@outlook.com>
2019-05-26 22:34:21 -0700

    model for lists and associate migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.http import HttpResponse
+from lists.models import Item
 
 class homePageTest(TestCase):
     def test_uses_home_template(self):
@@ -13,3 +14,20 @@ class homePageTest(TestCase):
         response = self.client.post('/', data={'item_text':'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        save_items = Item.objects.all()
+        self.assertEqual(save_items.count(), 2)
+
+        first_save_item = save_items[0]
+        second_save_item = save_items[1]
+        self.assertEqual(first_save_item.text, 'The first (ever) list item')
+        self.assertEqual(second_save_item.text, 'Item the second')
