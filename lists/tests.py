eff74292ac032a42aea9fbbc2152147242564e1e
elon <awesoempyc@outlook.com>
2019-06-08 08:15:52 -0700

    remove old post view

--- lists/tests.py
+++ lists/tests.py
@@ -10,18 +10,6 @@ class homePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text':'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        first_item = Item.objects.first()
-        self.assertEqual(first_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -39,9 +27,6 @@ class homePageTest(TestCase):
         self.assertEqual(first_save_item.text, 'The first (ever) list item')
         self.assertEqual(second_save_item.text, 'Item the second')
         
-    def text_only_save_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
     def test_display_all_items(self):
@@ -55,3 +40,15 @@ class ListViewTest(TestCase):
     def test_user_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
+    
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        first_item = Item.objects.first()
+        self.assertEqual(first_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,6 @@ from lists import views
 
 urlpatterns = [
     url('^$', views.home_page, name='home'),
+    url('^lists/new$', views.new_list, name='new_list'),
     url('^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
